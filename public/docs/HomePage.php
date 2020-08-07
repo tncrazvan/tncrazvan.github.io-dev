7da39f28347d66e7faa75a6180ebeafae2be9bd1
Razvan <tangent.jotey@gmail.com>
2020-08-07 22:22:52 +0200

    update

--- public/docs/HomePage.php
+++ public/docs/HomePage.php
@@ -6,7 +6,7 @@ use com\github\tncrazvan\catpaw\http\HttpEventHandler;
 use com\github\tncrazvan\catpaw\http\HttpEventOnClose;
 use com\github\tncrazvan\catpaw\tools\ServerFile;
 
-class HelloPage extends HttpEventHandler implements HttpMethodGet/*,HttpMethodPost,HttpMethodPut,...,HttpMethodUnknown*/{
+class HelloPage extends HttpEventHandler implements HttpMethodGet{
     private string $test;
     private HttpEvent $e;
     public function __construct(string $test, HttpEvent $e, ?HttpEventOnClose &$onClose = null){
